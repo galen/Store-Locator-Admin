f6189f0a40820e6eb09a1070017f0a638a8dfe1d
Galen Grover <galenjr@gmail.com>
2011-08-23 00:54:26 -0400

    Clean up geocode function

--- system/ajax/geocode.php
+++ system/ajax/geocode.php
@@ -5,6 +5,8 @@ require( DIR_LIB . '/PHPGoogleMaps/Core/Autoloader.php' );
 $map_loader = new SplClassLoader( 'PHPGoogleMaps', DIR_LIB );
 $map_loader->register();
 
+// This turns {address}, {city}, {state} from the config
+// into 123 main st, san diego, ca
 preg_match_all( '~{(.*?)}~', $config['geocode_string'], $gs );
 $geocode_string = $config['geocode_string'];
 foreach( $_GET as $k => $v ) {
@@ -14,6 +16,7 @@ foreach( $_GET as $k => $v ) {
 }
 
 $geocode = \PHPGoogleMaps\Service\Geocoder::geocode( $geocode_string );
+
 if ( $geocode instanceof \PHPGoogleMaps\Service\GeocodeResult ) {
 	if ( isset( $_GET['save_store'] ) ) {
 		require( DIR_SYSTEM . '/models/StoreTableGateway.php' );
@@ -21,12 +24,9 @@ if ( $geocode instanceof \PHPGoogleMaps\Service\GeocodeResult ) {
 		require( DIR_SYSTEM . '/core/Db.php' );
 		$db = Db::connect( DB_HOST, DB_USER, DB_PASSWORD, DB_NAME ) or die( json_encode( array( 'status' => 0, 'message' => 'Unable to connect to database' ) ) );
 		$stg = new StoreTableGateway( $db, STORE_LOCATIONS_TABLE, $config['column_map'] );
-		$vars['column_info'] = $stg->getColumns();
-		$vars['columns'] = array_keys( $vars['column_info'] );
-		$store_save = new Store( $config['column_map'], array( $config['column_map']['id'] => $_GET[$config['column_map']['id']], $config['column_map']['lat'] => $geocode->getLat(), $config['column_map']['lng'] => $geocode->getLng() ) );
-		$stg = new StoreTableGateway( $db, STORE_LOCATIONS_TABLE, $config['column_map'] );
-		if ( $stg->saveStore( $store_save ) ) {
-			$json = array( 'status' => 1, 'message' => 'Store saved successfully' );
+		$store = new Store( $config['column_map'], array( $config['column_map']['id'] => $_GET[$config['column_map']['id']], $config['column_map']['lat'] => $geocode->getLat(), $config['column_map']['lng'] => $geocode->getLng() ) );
+		if ( $stg->saveStore( $store ) ) {
+			$json = array( 'status' => 1, 'message' => 'Geocoding successful' );
 		}
 		else {
 			$json = array( 'status' => 0, 'message' => 'Error saving the store' );

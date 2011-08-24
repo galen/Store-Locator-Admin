6f4933f88b2aeadf885b4dcbd418b2a5d2493d61
Galen Grover <galenjr@gmail.com>
2011-08-24 01:24:21 -0400

    Remove phantom config files

--- system/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$routes = array (
-	'list(?:/page/(?<page_number>\d+))?'	=> 'list',
-	'search(?:/page/(?<page_number>\d+))?'	=> 'search',
-	'edit/(?<page_number>\d+)'				=> 'edit',
-	'delete/(?<page_number>\d+)'			=> 'delete'
-);

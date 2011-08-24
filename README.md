db38410af6d0aac186a75a6bd9e791a79d814e86
Galen Grover <galenjr@gmail.com>
2011-08-24 02:59:56 -0400

    Updated readme

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Store Locator Admin
 
 Store locator admin for the web
+
+##Features
+
+ - Easy integration with existing tables
+ 
+   Only requirements are:
+   
+    - Table must have an auto incrementing ID field
+    - Table must have separate lat/lng columns
+
+
+ - Geocoding and batch geocoding of stores
+ - Robust search capability
+ - Google map integration
+
+##Coming Soon
+
+ - Import/Backup functions
+ - HTML capabilities
+ - Images uploads
+
+Requires PHP 5.3+ with PDO
--- public/ajax/geocode.php
+++ public/ajax/geocode.php
@@ -17,8 +17,8 @@ if ( $geocode instanceof \PHPGoogleMaps\Service\GeocodeResult ) {
 		require( DIR_SYSTEM . '/models/StoreTableGateway.php' );
 		require( DIR_SYSTEM . '/models/Store.php' );
 		require( DIR_SYSTEM . '/core/Db.php' );
-		$db = Db::connect( DB_HOST, DB_USER, DB_PASSWORD, DB_NAME ) or die( json_encode( array( 'status' => 0, 'message' => 'Unable to connect to database' ) ) );
-		$stg = new StoreTableGateway( $db, STORE_LOCATIONS_TABLE, $config['column_map'] );
+		$db = $db = Db::connect( $config['db_host'], $config['db_user'], $config['db_password'], $config['db_name'] ) or die( json_encode( array( 'status' => 0, 'message' => 'Unable to connect to database' ) ) );
+		$stg = new StoreTableGateway( $db, $config['db_table'], $config['column_map'] );
 		$store = new Store( $config['column_map'], array( $config['column_map']['id'] => $_GET[$config['column_map']['id']], $config['column_map']['lat'] => $geocode->getLat(), $config['column_map']['lng'] => $geocode->getLng() ) );
 		if ( $stg->saveStore( $store ) ) {
 			$json = array( 'status' => 1, 'message' => 'Geocoding successful' );

06399ff0a3322ed25e34df48ce391f057b3bf131
Galen <galenjr@gmail.com>
2011-08-30 17:16:07 -0400

    CSS Improvements

--- system/views/widgets/store_listing.php
+++ system/views/widgets/store_listing.php
@@ -20,7 +20,7 @@
 			<td><?php e( $store->raw( $tc ) ) ?></td>
 		<?php endforeach; ?>
 		<td class="center"><?php if( $store->isGeocoded() ): ?>Yes<?php else: ?><a href="#" class="geocode_table btn small success" title="Geocode this store">No</a><?php endif; ?></td>
-		<td class="actions_cell center">
+		<td class="store_actions center">
 			<a class="btn small primary" href="<?php echo URL_EDIT ?>/<?php echo $store->getID() ?>/" title="Edit this store">edit</a>
 			<a class="btn small danger delete_store" href="<?php echo URL_DELETE ?>/<?php echo $store->getID() ?>/" data-id="<?php echo $store->getID() ?>" class="delete_store" title="Delete this store">X</a>
 		</td>

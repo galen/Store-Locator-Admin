eee0eed93f3328d76d590816b7b4fea955e66074
Galen <galenjr@gmail.com>
2011-09-02 01:00:03 -0400

    Checnge title

--- system/views/widgets/navigation.php
+++ system/views/widgets/navigation.php
@@ -2,7 +2,7 @@
 	<div class="topbar">
 		<div class="fill">
 			<div class="container">
-				<h3><a href="<?php echo URL_ROOT ?>">Store Admin</a></h3>
+				<h3><a href="<?php echo URL_ROOT ?>">Store Locator Admin</a></h3>
 				<ul>
 					<li><a href="<?php echo URL_ROOT ?>/list/">List</a></li>
 					<li><a href="<?php echo URL_ROOT ?>/search/">Search</a></li>

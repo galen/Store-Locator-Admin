045000f9ff8f8069986b77ae2daf6ff7d80169d7
Galen <galenjr@gmail.com>
2011-09-07 13:21:36 -0400

    Improve pagination code

--- system/controllers/list.php
+++ system/controllers/list.php
@@ -18,21 +18,14 @@ if ( $vars->page_number < 1 || ( $vars->page_number > $vars->total_pages && $var
 }
 
 // Pagination size error checking
-if ( $config['pagination_size'] % 2 == 0 ) {
-	$config['pagination_size']--;
+if ( $config['pagination_viewport'] < 0 || $config['pagination_viewport'] > 7 ) {
+	$config['pagination_viewport'] = 4;
 }
-if ( $config['pagination_size'] < 1 || $config['pagination_size'] > 12 ) {
-	$config['pagination_size'] = 9;
-}
-$vars->page_array = range( $vars->page_number - floor( $config['pagination_size']/2 ), $vars->page_number + floor( $config['pagination_size']/2 ) );
-if ( $vars->page_array[0] < 1 ) {
-	$end_page = end( $vars->page_array ) + abs( $vars->page_array[0] ) + 1;
-	$vars->page_array = range( 1, $end_page > $vars->total_pages ? $vars->total_pages : $end_page );
-}
-if ( end( $vars->page_array ) > $vars->total_pages ) {
-	$start_page = $vars->page_number - abs( $vars->total_pages - $vars->page_number - ( $config['pagination_size'] - 1 ) );
-	$vars->page_array = array_reverse( range( $vars->total_pages, $start_page < 1 ? 1 : $start_page ) );
-}
+
+$start_page = max( $vars->page_number - $config['pagination_viewport'], 1 ); // 22
+$end_page = min( $start_page + ( $config['pagination_viewport']*2 ), $vars->total_pages ); // 28
+$start_page = max( $end_page - ( $config['pagination_viewport']*2 ), 1 );
+$vars->page_array = range($start_page,$end_page);
 
 $vars->prev_page = $vars->page_number != 1 ? $vars->page_number - 1 : null;
 

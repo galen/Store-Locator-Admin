2ddf7d1bf6d0b9e83ac63330d2ae5bd9ec96500d
Galen <galenjr@gmail.com>
2011-09-15 19:46:01 -0400

    Update route file

--- system/config/routes.php
+++ system/config/routes.php
@@ -13,15 +13,8 @@ Router::connect( '~^tools$~', 'tools' );
 Router::connect( '~^export$~', 'export' );
 Router::connect( '~^documentation$~', 'documentation' );
 
+// Route for API requests
 Router::connect( '~^api(?:/(?P<sub_controller>\w+)/?(?P<store_id>\d*)?)?~', 'api' );
 
-/*
-Router::connect( '~^api$~', 'api/index' );
-Router::connect( '~^api/geocode/?(?P<all>all)?$~', 'api/geocode' );
-Router::connect( '~^api/edit/(?P<store_id>\d+)$~', 'api/edit' );
-Router::connect( '~^api/delete/(?P<store_id>\d+)$~', 'api/delete' );
-Router::connect( '~^api/get/(?P<store_id>\d+)$~', 'api/get' );
-Router::connect( '~^api/create$~', 'api/create' );
-*/
-
+// Default route
 Router::connect( '~^$~', 'list' );

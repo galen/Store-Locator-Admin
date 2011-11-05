997475d04e0c8cad601e291210eec31ebc137cbf
Galen <galenjr@gmail.com>
2011-11-05 15:24:13 -0700

    Add logo and favicon

--- system/views/widgets/navigation.php
+++ system/views/widgets/navigation.php
@@ -2,7 +2,7 @@
 	<div class="topbar">
 		<div class="fill">
 			<div class="container">
-				<h3><a href="<?php echo URL_ROOT ?>">Store Locator Admin</a></h3>
+				<h3 id="page_title"><a href="<?php echo URL_ROOT ?>" style="background-image:url(<?php e( URL_PUBLIC ) ?>/images/logo.png);">Store Locator Admin</a></h3>
 				<ul>
 					<li><a href="<?php echo URL_ROOT ?>/list/">List</a></li>
 					<li><a href="<?php echo URL_ROOT ?>/search/">Search</a></li>
